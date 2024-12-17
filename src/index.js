5a430e6ff2130b0fc6f8496549977f32cc7f4dac
salgado2004 <leosalgado2004@gmail.com>
2024-12-17 12:02:51 -0300

    fix: rename import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const utils = require("./util");
+const utils = require("./utils");
 
 async function run() {
     try {
