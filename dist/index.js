85a3e846b37bf5bf7c36aa92e3e4ed456959038a
salgado2004 <leosalgado2004@gmail.com>
2024-12-17 12:09:27 -0300

    refactor: remove utils file

--- dist/index.js
+++ dist/index.js
@@ -33903,75 +33903,6 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
-/***/ 2559:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-const sharp = __nccwpck_require__(5826)
-
-function findIndexes(data){
-    const startComment = '<!-- contributors -->';
-    const endComment = '<!-- /contributors -->';
-    const startIndex = data.indexOf(startComment) + startComment.length;
-    const endIndex = data.indexOf(endComment);
-
-    if(startIndex === -1 || endIndex === -1){
-        core.warning("Comment markers not found");
-        throw new Error("Unable to update README content");
-    }
-
-    if(startIndex >= endIndex){
-        core.warning("Comment markers misplaced");
-        throw new Error("Unable to update README content");
-    }
-
-    return [startIndex, endIndex];
-}
-
-async function processImage(path){
-    const roundedCorners = Buffer.from(
-        `<svg><rect x="0" y="0" width="100" height="100" rx="40" ry="40" /></svg>`
-    );
-
-    const buffer = await sharp(path)
-    .resize(100,100)
-    .composite([{ input: roundedCorners, blend: 'dest-in' }])
-    .sharpen()
-    .withMetadata()
-    .toBuffer();
-
-    const base64Image = buffer.toString('base64');
-    const dataUrl = `data:image/png;base64,${base64Image}`;
-
-    return dataUrl;
-}
-
-async function createChart(contributorsList){
-    let contributorsChart = "<table>\n\t<tr>\n";
-
-    for(let contributor of contributorsList) {
-        const path = await processImage(contributor[1]);
-        contributorsChart += 
-`       <td align="center">
-            <a href="${contributor[2]}">
-                <img src="${path}" alt="${contributor[0]}"/>
-                <p><strong>${contributor[0]}</strong></p>
-            </a>
-        </td>
-`
-    };
-
-    contributorsChart += "\t</tr>\n</table>";
-
-    return contributorsChart;
-}
-
-module.exports = {
-    findIndexes,
-    createChart
-}
-
-/***/ }),
-
 /***/ 5244:
 /***/ ((module) => {
 
@@ -41648,16 +41579,73 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"sharp","description":"High pe
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const src_core = __nccwpck_require__(3063);
+const core = __nccwpck_require__(3063);
 const github = __nccwpck_require__(2083);
-const utils = __nccwpck_require__(2559);
+const sharp = __nccwpck_require__(5826);
+
+function findIndexes(data){
+    const startComment = '<!-- contributors -->';
+    const endComment = '<!-- /contributors -->';
+    const startIndex = data.indexOf(startComment) + startComment.length;
+    const endIndex = data.indexOf(endComment);
+
+    if(startIndex === -1 || endIndex === -1){
+        core.warning("Comment markers not found");
+        throw new Error("Unable to update README content");
+    }
+
+    if(startIndex >= endIndex){
+        core.warning("Comment markers misplaced");
+        throw new Error("Unable to update README content");
+    }
+
+    return [startIndex, endIndex];
+}
+
+async function processImage(path){
+    const roundedCorners = Buffer.from(
+        `<svg><rect x="0" y="0" width="100" height="100" rx="40" ry="40" /></svg>`
+    );
+
+    const buffer = await sharp(path)
+    .resize(100,100)
+    .composite([{ input: roundedCorners, blend: 'dest-in' }])
+    .sharpen()
+    .withMetadata()
+    .toBuffer();
+
+    const base64Image = buffer.toString('base64');
+    const dataUrl = `data:image/png;base64,${base64Image}`;
+
+    return dataUrl;
+}
+
+async function createChart(contributorsList){
+    let contributorsChart = "<table>\n\t<tr>\n";
+
+    for(let contributor of contributorsList) {
+        const path = await processImage(contributor[1]);
+        contributorsChart += 
+`       <td align="center">
+            <a href="${contributor[2]}">
+                <img src="${path}" alt="${contributor[0]}"/>
+                <p><strong>${contributor[0]}</strong></p>
+            </a>
+        </td>
+`
+    };
+
+    contributorsChart += "\t</tr>\n</table>";
+
+    return contributorsChart;
+}
 
 async function run() {
     try {
-        const token = src_core.getInput('token');
+        const token = core.getInput('token');
         const octokit = github.getOctokit(token);
 
-        src_core.info("Request README data");
+        core.info("Request README data");
 
         const readme = await octokit.rest.repos.getReadme({
             owner: github.context.repo.owner,
@@ -41671,21 +41659,21 @@ async function run() {
             repo: github.context.repo.repo
         });
 
-        src_core.info("Gather contributors list");
+        core.info("Gather contributors list");
 
         const contributorsList = contributors.data.map(contributor => [contributor.login, contributor.avatar_url, contributor.html_url]);
 
-        const contributorsChart = utils.createChart(contributorsList);
+        const contributorsChart = createChart(contributorsList);
 
-        const indexes = utils.findIndexes(content);
+        const indexes = findIndexes(content);
 
-        src_core.info("Update README content");
+        core.info("Update README content");
 
         const newContent = content.slice(0, indexes[0]) + "\n" + contributorsChart + "\n" + content.slice(indexes[1]);
 
         const contentEncoded = Buffer.from(newContent).toString('base64');
 
-        src_core.info("Commit updates");
+        core.info("Commit updates");
 
         const { data } = await octokit.rest.repos.createOrUpdateFileContents({
             owner: github.context.repo.owner,
@@ -41697,7 +41685,7 @@ async function run() {
         });
 
     } catch (error) {
-        src_core.setFailed(error.message);
+        core.setFailed(error.message);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const utils = require("./utils");
+const sharp = require("sharp");
+
+function findIndexes(data){
+    const startComment = '<!-- contributors -->';
+    const endComment = '<!-- /contributors -->';
+    const startIndex = data.indexOf(startComment) + startComment.length;
+    const endIndex = data.indexOf(endComment);
+
+    if(startIndex === -1 || endIndex === -1){
+        core.warning("Comment markers not found");
+        throw new Error("Unable to update README content");
+    }
+
+    if(startIndex >= endIndex){
+        core.warning("Comment markers misplaced");
+        throw new Error("Unable to update README content");
+    }
+
+    return [startIndex, endIndex];
+}
+
+async function processImage(path){
+    const roundedCorners = Buffer.from(
+        `<svg><rect x="0" y="0" width="100" height="100" rx="40" ry="40" /></svg>`
+    );
+
+    const buffer = await sharp(path)
+    .resize(100,100)
+    .composite([{ input: roundedCorners, blend: 'dest-in' }])
+    .sharpen()
+    .withMetadata()
+    .toBuffer();
+
+    const base64Image = buffer.toString('base64');
+    const dataUrl = `data:image/png;base64,${base64Image}`;
+
+    return dataUrl;
+}
+
+async function createChart(contributorsList){
+    let contributorsChart = "<table>\n\t<tr>\n";
+
+    for(let contributor of contributorsList) {
+        const path = await processImage(contributor[1]);
+        contributorsChart += 
+`       <td align="center">
+            <a href="${contributor[2]}">
+                <img src="${path}" alt="${contributor[0]}"/>
+                <p><strong>${contributor[0]}</strong></p>
+            </a>
+        </td>
+`
+    };
+
+    contributorsChart += "\t</tr>\n</table>";
+
+    return contributorsChart;
+}
 
 async function run() {
     try {
@@ -25,9 +82,9 @@ async function run() {
 
         const contributorsList = contributors.data.map(contributor => [contributor.login, contributor.avatar_url, contributor.html_url]);
 
-        const contributorsChart = utils.createChart(contributorsList);
+        const contributorsChart = createChart(contributorsList);
 
-        const indexes = utils.findIndexes(content);
+        const indexes = findIndexes(content);
 
         core.info("Update README content");
 
